aa3c7745366bbfdfa3f9b6268411ee3c604eec6c
Marina Hany Amine <63201349+meraamine@users.noreply.github.com>
2026-09-08 21:16:31 +0300

    Enhance SearchTest with ArticlePage functionality
    Added ArticlePage interactions to the search test.

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import base.BaseTest;
 import org.testng.annotations.Test;
+import pages.ArticlePage;
 import pages.HomePage;
 import pages.SearchPage;
 
@@ -12,13 +13,15 @@ public class SearchTest extends BaseTest {
 
         HomePage homePage = new HomePage(driver);
         SearchPage searchPage = new SearchPage(driver);
+        ArticlePage articlePage = new ArticlePage(driver);
 
         homePage.clickSearch();
-
         searchPage.closePopup();
-
         searchPage.clickSearchField();
         searchPage.enterSearchText("Artificial Intelligence");
         searchPage.clickFirstSearchResult();
+        articlePage.closePopup();
+        articlePage.clickSave();
+
     }
 }
